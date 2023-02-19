f17c780e94c9f4e9b751696329ce5b837de368be
hafizhsyafiqh <hafizhsyafiqh@gmail.com>
2023-02-19 16:01:09 +0700

    first commit

---
+++ backend/src/main/java/com/syafiqh/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.syafiqh.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not found the user with id "+ id);
+    }
+}
